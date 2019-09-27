73564d2bad1ff3358f9a1ad80f17252dd896fe11
Satlok <satlok.gupta@jithvar.com>
2019-09-27 22:05:19 +0530

    front layout

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -25,7 +25,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin-home');
+        return view('admin.dashboard');
     }
 
     public function dashboard()
