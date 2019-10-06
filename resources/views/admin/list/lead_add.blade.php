cfd63cc1193a28ef57bec1b9fd058ec8382f91d7
Satlok <satlok.gupta@jithvar.com>
2019-10-07 00:24:33 +0530

    Lead Management

--- resources/views/admin/list/lead_add.blade.php
+++ resources/views/admin/list/lead_add.blade.php
@@ -5,12 +5,12 @@
  @section('bread')
  <section class="content-header">
       <h1>
-        Keyword
+        Lead Management
       
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Admin</a></li>
-        <li class="active">Keyword</li>
+        <li class="active">Lead Management</li>
       </ol>
 @endsection
 
